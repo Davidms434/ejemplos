d670e5d303dff6eafbcf1ee3357e9ff7e5b073ef
davidms434 <dmendiet@emeal.nttdata.com>
2022-09-15 13:42:24 -0500

    primera versión

---
+++ example/example.rs
@@ -0,0 +1,71 @@
+//use rand::{Rng, thread_rng};
+
+
+fn main(){
+
+       /*let mut& x:u32 = 3;
+    
+    println!("la v&ariable es: {}",x);
+     */
+   // x = 7;
+   // println!("la variable es:{}",x);
+
+   // const  PI: f32 = 3.14;
+   // println!("la variable es:{}",PI);
+
+    //let x = "a";
+   // println!("la variable es:{}",x);
+
+   // let decimal1 = 3.4;
+    //let decimal2: f32 = 3333.2;
+
+// Tuplas
+
+    //let tupla = (500,80,20);
+    //let (x,y,z) = tupla;
+    //println!("El segundo valor de la tupla es: {}", y);
+
+    //MATRIZ
+
+    //let matriz = [500,80,20];
+    //let x = matriz [2];
+   // println!("El segundo valor de la tupla es: {}", x);
+
+    //Funciones que devuelven valor
+
+
+
+//let i = trece();
+ //   println!("el valor es {}", i)
+
+//}
+
+//fn trece() -> i8 {
+    
+//13
+//return 13;
+
+//EJEMPLOS PRINT
+   /*  println!("{a}, this is {b}. {b}, this is {a}",
+        b = "Bob",
+        a = "Alice"         
+        );
+        */
+
+     // println!("{0}, this is {1}. {1}, this is {0}", "Bob", "Alice");
+
+     
+
+     /// <p style="padding: 8 16 16 16; padding-bottom: 8; color: #ffffff; background: #8888aa;"><b>Método para generar una clave.</b></p>
+     ///
+     /// # <b><u>Argumentos</u></b>
+     ///
+     /// * longitud - Longitud de la clave.
+     ///
+     /// # <b><u>Resultado</u></b>
+     ///
+     /// Texto con la clave según la longitud dada.
+     ///
+    
+    println!("{0}, this is {1}. {1}, this is {0}","Alice", "Bob");
+}
